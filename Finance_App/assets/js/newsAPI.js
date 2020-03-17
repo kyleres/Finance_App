aa236f350ffb8e4c3d65e8568faa8d5e8c504747
Alex B <abailon949@gmail.com>
2020-03-17 09:24:45 -0700

    updated newsAPI to display on html

--- Finance_App/assets/js/newsAPI.js
+++ Finance_App/assets/js/newsAPI.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
     $("#searchBtn").on("click", function() {
+        event.preventDefault();
 
         var stock = $("#searchStock").val();
         var settings = {
